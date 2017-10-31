5bc5180483772bcebfa4cbf7c720ace3c56ff329
tayrih <tahysluana@gmail.com>
2017-10-31 12:18:48 -0500

    Añade interaccion al header

--- js/header.js
+++ js/header.js
@@ -5,10 +5,12 @@ $(function() {
     var scrollTop = $(document).scrollTop();
     if (scrollTop > menuOffset.top) {
       $('#menu-nav').addClass('menu-white');
-      $('#menu-nav').removeClass('menu-trasparent');
+      $('#menu-nav').removeClass('menu-transparent');
+      document.getElementById('logoimg').src = "assets/images/logo-pink.png";
     } else {
       $('#menu-nav').removeClass('menu-white');
-      $('#menu-nav').addClass('menu-trasparent');
+      $('#menu-nav').addClass('menu-transparent');
+      document.getElementById('logoimg').src = "assets/images/logo-white.png";
     }
   });
 
